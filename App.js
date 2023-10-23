e4f359ccc763b4a5867a2487a31f787c2a70e893
Castillo <dcastillo10@ucol.mx>
2023-10-22 18:17:23 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { Button, Switch, StyleSheet,TextInput,Alert, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue: false,
+      inputTextValue:""
+    }
+  }
+  alertToInputText = () =>
+  Alert.alert(`${this.state.inputTextValue}`, 'Escrito', [
+    {text: 'OK', onPress: () => console.log('OK')},
+  ]);
+
+
+  onChange = (value) => {
+    console.warn(`El switch cambiara a ${value}`)
+    this.setState({switchValue: value})
+  }
+  onChangeText = (value)=>{
+    this.setState({inputTextValue: value})
+  }
+  onPressLearnMore = () => {
+    console.warn(`Escribio: ${this.state.inputTextValue}`)
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={value => this.onChangeText(value)}
+          value={this.state.inputTextValue}
+          placeholder="Ingresa texto"
+        />
+        <Switch
+          onValueChange={()=>{this.onChange(!this.state.switchValue)}}
+          value={this.state.switchValue}
+        />
+        <Button
+          onPress={this.alertToInputText}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +57,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },  
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
